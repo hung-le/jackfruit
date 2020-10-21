e5483da3e3398cdbe34edb1942162daea76370c0
hung-le <none>
2020-10-21 15:54:49 -0700

    Add packaging tools.

---
+++ misc/jpackage.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+dest=../target/jpackage
+
+rm -rf ${dest}
+
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-14.0.2.jdk/Contents/Home
+
+jpackage=/Library/Java/JavaVirtualMachines/jdk-14.0.2.jdk/Contents/Home/bin/jpackage
+
+${jpackage} \
+  --add-modules java.base,java.logging,java.xml,java.sql,java.prefs,java.desktop,java.management,java.naming \
+  --type pkg \
+  --dest ${dest} \
+  --verbose \
+  -i ./target/ \
+  --name jackfruit \
+  --main-jar jackfruit-0.0.1-SNAPSHOT.jar
